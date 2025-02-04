f9c919d0b2cdcbe0aae7c360f934dc57d70c7d9e
Tathya Garg <tathya.garg@gmail.com>
2025-02-04 21:59:41 +0530

    removed tempaltes

--- src/handlers/get_ep.c
+++
@@ -1,6 +0,0 @@
-#include "../../include/request.h"
-#include <stdio.h>
-
-void get_get_ep(struct Request *request, struct Response *response) {
-  file_response("templates/get_ep.html", response, MIME_TEXT_HTML);
-}
